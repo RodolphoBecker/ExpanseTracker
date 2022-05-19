9cd14f521baf42359d1c593d1264580d7d20c18c
Rodolpho Becker <becker.rodolpho@gmail.com>
2022-05-19 08:32:34 -0300

    ✨ TransactionCard and CategoryCard components created

--- src/components/homeComponents/HomeProfile.jsx
+++ src/components/homeComponents/HomeProfile.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import styles from "../../styles/homeComponents/HomeProfile.module.scss";
+import { BsPerson, BsPencil, BsWallet2 } from "react-icons/bs";
+import { IoSettingsOutline } from "react-icons/io5";
+
+import { Link } from "react-router-dom";
+
+//UTILS
+// import { useUser } from "../../queries/user";
 
 const HomeProfile = () => {
-  return (
-    <div>HomeProfile</div>
-  )
-}
+	// const { data } = useUser();
+	return (
+		<div className={styles.container}>
+			<div className={styles.iconContainer}>
+				<BsPerson />
+			</div>
+			<div className={styles.info}>
+				<span className={styles.welcome}>
+					{/* {data && `Hi ${data?.data.firstName}!`} */}
+				</span>
+				<div className={styles.options}>
+					<Link to="profile">
+						<span>Profile</span>
+						<BsPencil />
+					</Link>
+					<Link to="settings">
+						<span>Settings</span>
+						<IoSettingsOutline />
+					</Link>
+					<Link to="wallet">
+						<span>Wallet</span>
+						<BsWallet2 />
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default HomeProfile
+export default HomeProfile;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import styles from "../styles/homeComponents/Home.module.scss";
 import MainContainer from "../components/Containers/MainContainer";
 import SearchBar from "../components/homeComponents/SearchBar";
+import CategoryCard from "../components/Cards/CategoryCard"
+import TransactionCard from "../components/Cards/TransactionCard";
+import HomeProfile from "../components/homeComponents/HomeProfile";
 import { Title } from "../components/Titles/Titles";
 
 const Home = () => {
@@ -12,8 +15,25 @@ const Home = () => {
 				</div>
 				<div className={styles.categories}>
 					<Title>Categories Last 30 Days</Title>
-					<div className={styles.content}>{/*categories*/}</div>
+					<div className={styles.content}>
+						<CategoryCard />
+						<CategoryCard category={"Products"} />
+						<CategoryCard />
+						<CategoryCard />
+					</div>
 				</div>
+				<div className={styles.transactions}>
+					<Title>Latest Transactions</Title>
+					<div className={styles.content}>
+						<TransactionCard />
+						<TransactionCard category={"Entertainment"} />
+						<TransactionCard category={"Bills"} />
+						<TransactionCard />
+					</div>
+				</div>
+			</div>
+			<div className={styles.profile}>
+				<HomeProfile />
 			</div>
 		</MainContainer>
 	);
